12d5baa223f550b14d9f7dae2c2998e7b37bf4a8
Erick Carrillo <erickcarrillo8@gmail.com>
2016-07-22 00:17:52 +0000

    citas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722001304) do
+ActiveRecord::Schema.define(version: 20160722001653) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "fecha"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dentists", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

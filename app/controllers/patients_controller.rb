f8d5c12002adebc51f6c52f59945a09763d258c0
Erick Carrillo <erickcarrillo8@gmail.com>
2016-07-26 04:09:29 +0000

    crud completo

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -12,4 +12,75 @@ class PatientsController < ApplicationController
     def new 
         @paciente = Patient.new
     end
+    
+    def create 
+        @paciente = Patient.new(patient_params)
+        
+        @paciente.save  
+        
+        redirect_to @paciente
+    end
+    
+    def edit
+        @paciente = Patient.find(params[:id])
+    end
+    
+    def update
+        @paciente = Patient.find(params[:id])
+        if @paciente.update(patient_params)
+            redirect_to @paciente
+        else
+            render :edit
+        end
+        
+    end
+    
+    
+    #DELETE /patients/:id
+    def destroy
+        @paciente = Patient.find(params[:id])
+        @paciente.destroy
+        redirect_to patients_path
+    end
+    
+    
+    private
+    def patient_params
+        params.require(:patient).permit(
+                                        :nombre,
+                                        :apellido_pat,
+                                        :apellido_mat, 
+                                        :direccion,
+                                        :telefono,
+                                        :celular,
+                                        :RFC,
+                                        :fecha_nac,
+                                        :religion,
+                                        :buen_estado_saludo ,
+                                        :alergico_sustancia,
+                                        :problemas_anestesia ,
+                                        :hemorragia ,
+                                        :embarazo ,
+                                        :diabates ,
+                                        :hipertencion ,
+                                        :fiebre_reumatica ,
+                                        :asma ,
+                                        :enfermades_respiratoras,
+                                        :enfermedades_cardiovasculares, 
+                                        :dolor_boca ,
+                                        :sangrado_encia ,
+                                        :mal_olor_boca ,
+                                        :dientes_moviles ,
+                                        :dolor_abrir_cerrar_boca ,
+                                        :malos_habitos ,
+                                        :satisfecho ,
+                                        :ultima_visita ,
+                                        :tratamiento_adquirido ,
+                                        :motivo_consulta ,
+                                        :autorizacion
+                                        )
+    end
+    
+    
+
 end

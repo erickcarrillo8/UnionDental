155ead5321aa87d589c469b34267967746234bf9
Erick Carrillo <erickcarrillo8@gmail.com>
2016-07-29 01:18:45 +0000

    relaciones diagnosis paciente y tratamiento

--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -1,2 +1,4 @@
 class Diagnosis < ActiveRecord::Base
+    belongs_to :dentist
+    belongs_to :patient
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,2 +1,5 @@
 class Treatment < ActiveRecord::Base
+    belongs_to :dentist
+    belongs_to :patient
+    belongs_to :diagnoses
 end

bc2aad6bd56ef3c2b167c0c1054292dc51f2f799
Erick Carrillo <erickcarrillo8@gmail.com>
2016-08-02 08:19:17 +0000

    eliminando migraciones

--- db/migrate/20160729073013_rename_table_diagnoses_to_diagnosis.rb
+++
@@ -1,5 +0,0 @@
-class RenameTableDiagnosesToDiagnosis < ActiveRecord::Migration
-  def change
-    rename_table :diagnoses, :diagnosis
-  end
-end
--- db/migrate/20160729073708_change_name_diagnoses_id_top_diagnosis_id.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNameDiagnosesIdTopDiagnosisId < ActiveRecord::Migration
-  def change
-     rename_column :treatments, :diagnoses_id, :diagnosis_id
-  end
-end

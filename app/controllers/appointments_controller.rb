28fe15027503801ee884013af5ec325e9ad62d3f
Erick Carrillo <erickcarrillo8@gmail.com>
2016-09-02 06:30:52 +0000

    forma añadida

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,25 @@ class AppointmentsController < ApplicationController
     before_action :authenticate_dentist!
     
     def index
+        @citas = Appointment.all 
+        @cita = Appointment.new
+    end 
+    
+    def new
+       
+       
+    end 
+    
+    
+    def create
         
+    end 
+    
+    
+    private 
+    
+    def set_patient
+        @paciente = Patient.find(params[:patient_id])
+          
     end 
 end
